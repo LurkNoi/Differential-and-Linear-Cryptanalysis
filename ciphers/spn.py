71bb599a3a601f122d636a1ab51b9acae5b56a17
LurkNoi <lurk061012@gmail.com>
2020-04-28 15:59:40 +0800

    add PRESENT-80 cipher

--- ciphers/spn.py
+++ ciphers/spn.py
@@ -80,6 +80,12 @@ class SPN:
         block_i ^= subkey_i
         return block_i & self.block_mask
 
+    def encrypt_block(self, block):
+        raise NotImplementedError
+
+    def decrypt_block(self, block):
+        raise NotImplementedError
+
     def encrypt(self, data):
         block_size = self.block_size
         if len(data) % block_size != 0:
@@ -127,7 +133,7 @@ class basicSPN(SPN):
             ))
         self.KEY = key
         self.subkey_ = [byte_to_int(key[i:i+block_size])
-                  for i in range(0, len(key), block_size)]
+                        for i in range(0, len(key), block_size)]
         SPN.__init__(self, sbox=self.SBOX, pbox=self.PBOX,
                      nrounds=nrounds, block_size=block_size)
 
@@ -161,14 +167,89 @@ class basicSPN(SPN):
         return int_to_byte(block_i, self.block_size)
 
 
+class PRESENT_80(SPN):
+    """
+    PRESENT-80 CIPHER
+    """
+
+    SBOX = [0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD,
+            0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2]
+    PBOX = [
+        0x00, 0x10, 0x20, 0x30, 0x01, 0x11, 0x21, 0x31,
+        0x02, 0x12, 0x22, 0x32, 0x03, 0x13, 0x23, 0x33,
+        0x04, 0x14, 0x24, 0x34, 0x05, 0x15, 0x25, 0x35,
+        0x06, 0x16, 0x26, 0x36, 0x07, 0x17, 0x27, 0x37,
+        0x08, 0x18, 0x28, 0x38, 0x09, 0x19, 0x29, 0x39, 
+        0x0a, 0x1a, 0x2a, 0x3a, 0x0b, 0x1b, 0x2b, 0x3b,
+        0x0c, 0x1c, 0x2c, 0x3c, 0x0d, 0x1d, 0x2d, 0x3d,
+        0x0e, 0x1e, 0x2e, 0x3e, 0x0f, 0x1f, 0x2f, 0x3f,
+    ]
+
+    def __init__(self, key, nrounds=31):
+        block_size = 8
+        if len(key) != 10:
+            raise TypeError('key length must be 10')
+        self.KEY = key
+        self.subkey_ = self.key_schedule(key, nrounds)
+        SPN.__init__(self, sbox=self.SBOX, pbox=self.PBOX,
+                     nrounds=nrounds, block_size=block_size)
+
+    def __repr__(self):
+        return "PRESENT-80 ({} rounds) with key = {}".format(self.Nr, self.KEY.hex())
+
+    def key_schedule(self, key, nrounds):
+        Sbox = self.SBOX
+        key_i = byte_to_int(key)
+        roundkeys = []
+        for i in range(1, nrounds+1):
+            roundkeys.append(key_i >> 16)
+            key_i = ((key_i & (2**19-1)) << 61) + (key_i >> 19)
+            key_i = (Sbox[key_i >> 76] << 76) + (key_i & (2**76-1))
+            key_i ^= i << 15
+        return roundkeys
+
+    def encrypt_block(self, block):
+        block_i = byte_to_int(block) & self.block_mask
+        Nr = self.Nr
+        subkey_ = self.subkey_
+        for r in range(Nr - 1):
+            block_i = self.key_mixing(block_i, subkey_[r])
+            block_i = self.substitution(block_i, MODE_ENC)
+            block_i = self.permutation(block_i, MODE_ENC)
+        block_i = self.key_mixing(block_i, subkey_[Nr-1])
+        return int_to_byte(block_i, self.block_size)
+
+    def decrypt_block(self, block):
+        block_i = byte_to_int(block) & self.block_mask
+        Nr = self.Nr
+        subkey_ = self.subkey_
+        block_i = self.key_mixing(block_i, subkey_[Nr-1])
+        for r in range(Nr-2, -1, -1):
+            block_i = self.permutation(block_i, MODE_DEC)
+            block_i = self.substitution(block_i, MODE_DEC)
+            block_i = self.key_mixing(block_i, subkey_[r])
+        return int_to_byte(block_i, self.block_size)
+
+
 if __name__ == '__main__':
     import os
 
     nrounds = 5
     key = os.urandom(2*nrounds + 2)
-    SPN_CIPHER = basicSPN(key, nrounds=nrounds)
+    cipher = basicSPN(key, nrounds=nrounds)
+    print(cipher)
     plaintext = os.urandom(10)
-    ciphertext = SPN_CIPHER.encrypt(plaintext)
-    if SPN_CIPHER.decrypt(ciphertext) != plaintext:
+    ciphertext = cipher.encrypt(plaintext)
+    if cipher.decrypt(ciphertext) != plaintext:
         raise ValueError("sth wrong")
-    print("PASS")
+    print("  PASS")
+
+    nrounds = 32
+    key = os.urandom(10)
+    cipher = PRESENT_80(key, nrounds=nrounds)
+    print(cipher)
+    plaintext = os.urandom(16)
+    ciphertext = cipher.encrypt(plaintext)
+    if cipher.decrypt(ciphertext) != plaintext:
+        raise ValueError("sth wrong")
+    print("  PASS")
